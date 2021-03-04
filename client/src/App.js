3b7c4f82ce34d9e750acc38812cfa75c0ee34757
marie-Eve Dubé <mariedube2@gmail.com>
2021-03-03 20:22:21 -0500

    First iteration on page subdivision *sidebard + garden view *title, tools, filters, and list

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,62 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+class TitleSection extends Component {
+  render() {
+    return <div className="TitleSection">TitleSection</div>;
+  }
+}
+
+class ToolsSection extends Component {
+  render() {
+    return <div className="ToolsSection">ToolsSection</div>;
+  }
+}
+
+class FilterSection extends Component {
+  render() {
+    return (
+      <div className="FilterSection">
+        FilterSection
+        <button>A</button>
+        <button>B</button>
+        <button>C</button>
+        <button>D</button>
+      </div>
+    );
+  }
+}
+
+class ListSection extends Component {
+  render() {
+    return <div className="ListSection">ListSection</div>;
+  }
+}
+
+class Sidebar extends Component {
+  render() {
+    return (
+      <div className="Sidebar">
+        <TitleSection />
+        <ToolsSection />
+        <FilterSection />
+        <ListSection />
+      </div>
+    );
+  }
+}
+
+class GardenView extends Component {
+  render() {
+    return <div className="GardenView">GARDEN</div>;
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -20,7 +71,9 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
+        <Sidebar />
+        <GardenView />
       </div>
     );
   }
