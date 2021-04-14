37d9eae64842ab8560a2d8ae183212950f69fcb1
marie-Eve Dubé <mariedube2@gmail.com>
2021-04-14 19:36:11 -0400

    First version of canvas with hearts <3 <3 <3

--- client/src/GardenView.js
+++ client/src/GardenView.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
-
+import { useCanvas } from './hooks/useCanvas';
 import "./GardenView.css";
 
-class GardenView extends Component {
-  render() {
-    return <div className="garden-view">GARDEN</div>;
-  }
+function GardenView () {
+
+  const [ coordinates, setCoordinates, canvasRef, canvasWidth, canvasHeight ] = useCanvas();
+  
+  const handleCanvasClick=(event)=>{
+    // on each click get current mouse location 
+    const currentCoord = { x: event.clientX, y: event.clientY };
+    // add the newest mouse location to an array in state 
+    setCoordinates([...coordinates, currentCoord]);
+  };
+  return <div className="garden-view">
+      GARDEN
+      <canvas 
+        className="App-canvas"
+        ref={canvasRef}
+        width={canvasWidth}
+        height={canvasHeight}
+        onClick={handleCanvasClick} />
+    </div>;
 }
 
 export default GardenView;
