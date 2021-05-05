0fee2edd48a1b798d4aed82bc901a9357aa05cad
Hugo Bedard <hugo_bedard@hotmail.com>
2021-05-05 19:08:55 -0400

    Script to populate the db with plants.json

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -1,7 +1,9 @@
 
-@echo oFF
+@echo OFF
 
-DIR=%cd%
+SET "DIR=%cd%"
+
+:: Install Javascript dependencies
 
 cd "%~dp0..\client"
 CALL npm install
@@ -9,4 +11,11 @@ CALL npm install
 cd "%~dp0..\server"
 CALL npm install
 
+:: Setup Docker
+
+CALL docker-compose up -d
+
+:: Create Database
+
 cd %DIR%
+CALL docker exec -i garden-planner_db_1 psql -U postgres -d plants < .\db\1_db.sql
